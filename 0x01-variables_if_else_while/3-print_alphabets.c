2f81cef27265138678bc251a6c59985f5876f303
Awokay56 <kgbenga56@gmail.com>
2023-06-17 09:08:55 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main = Entry point
+ * Return: Eachpoint 0
+ */
+int main(void)
+{
+	char b;
+	char c;
+
+	b = 'a';
+	c = 'A'
+	while
+		(b <= 'a') {
+			putchar(b);
+			b++;
+		}
+	while
+		(c <= 'A') {
+			putchar(d);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
+
+
