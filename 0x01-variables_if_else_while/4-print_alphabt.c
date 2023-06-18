55d340cb836fc2e503bcd8a7452b8f5e9848b6c1
Awokay56 <kgbenga56@gmail.com>
2023-06-17 20:29:57 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main -prints the alphabet in lowercase
+ * Return: Eachpoint 0
+ */
+int main(void)
+{
+	char e;
+
+	e = 'a'';
+		while
+			(e <= 'z') {
+				if(e!='e' && e!='q')
+				{
+					putchar(e);
+				}
+				e++;
+			}
+		putchar('\n');
+		return (0);
+}
+
