4ab631f2d586dd03b467cc662fab3272e87e596b
Awokay56 <kgbenga56@gmail.com>
2023-06-18 19:50:35 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+# include <stdio.h>
+/**
+ * main -write a script that print puts
+ * Return: Each point 0
+ */
+int main(void)
+{
+	puts(*\"Programming is like building a multilingual puzzle");
+	return (0);
+}
