9bb9c029deec5154b4893600858dafa3c61f5914
Awokay56 <kgbenga56@gmail.com>
2023-06-21 19:59:57 -0700

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main -checks for lowercase character.
+ * Return:1 if c is lowercase or 0 for anything else
+ */
+
+int -islower.c(int c)
+
+{
+
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+		return (0);
+}
+
+
