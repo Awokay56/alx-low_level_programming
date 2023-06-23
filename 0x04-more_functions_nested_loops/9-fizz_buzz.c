771f957e5b96c6632c4273a70c130aca80a39287
Awokay56 <kgbenga56@gmail.com>
2023-06-23 08:00:02 -0700

    9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* main -Entry point
+* Description print Fizz instead of the number and for the multiples of five
+* Return: Eachpoint 0
+*/
+
+int main(void)
+{
+int i;
+
+for (i = 1; i <= 100; i++)
+{
+if (i % 15 == 0)
+printf("FizzBuzz");
+else if (i % 3 == 0)
+printf("FIZZ");
+else if (i% 5 == 0)
+printf("BUZZ");
+else
+printf(" % i", i);
+if (i < 100)
+printf(" ");
+}
+printf("\n");
+return (0);
+}
